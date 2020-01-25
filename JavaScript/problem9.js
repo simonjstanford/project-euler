d02054e7395420b07ce00fd7beff8d73104e24f7
Simon Stanford <simonjstanford@gmail.com>
2020-01-25 20:36:38 +0000

    Performance improvements

--- JavaScript/problem9.js
+++ JavaScript/problem9.js
@@ -3,8 +3,8 @@ const sumSqRoot = Math.sqrt(x);
 let answer = 0;
 
 //Euclid's formula
-for (let m = 0; m < sumSqRoot; m++) {
-    for (let n = 0; n < sumSqRoot; n++) {
+for (let m = 1; m < sumSqRoot; m++) {
+    for (let n = 1; n < m; n++) {
         let a = Math.pow(m, 2) - Math.pow(n, 2);
         let b = 2 * m * n;
         let c = Math.pow(m, 2) + Math.pow(n, 2);
