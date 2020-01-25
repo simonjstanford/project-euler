52a66276bce5b0b67c10044f869dd7651826d52f
Simon Stanford <simonjstanford@gmail.com>
2020-01-25 19:25:11 +0000

    Added comment

--- JavaScript/problem9.js
+++ JavaScript/problem9.js
@@ -2,6 +2,7 @@ const x = 1000;
 const sumSqRoot = Math.sqrt(x);
 let answer = 0;
 
+//Euclid's formula
 for (let m = 0; m < sumSqRoot; m++) {
     for (let n = 0; n < sumSqRoot; n++) {
         let a = Math.pow(m, 2) - Math.pow(n, 2);
